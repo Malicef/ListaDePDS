604efd8ca05efe40d639cd20e869d396e85d3bdf
João Pedro <116277457+Joao11110@users.noreply.github.com>
2024-09-26 08:17:57 -0300

    Create XMLReader.java

---
+++ factoryQ3/XMLReader.java
@@ -0,0 +1,9 @@
+import java.io.InputStream;
+import java.io.IOException;
+import org.xml.sax.SAXException;
+import org.xml.sax.ContentHandler;
+
+public interface XMLReader {
+    void setContentHandler(ContentHandler handler);
+    void parse(InputStream is) throws IOException, SAXException;
+}
